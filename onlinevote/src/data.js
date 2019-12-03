3422465776f9632952a57ca2dfb5957d3e166951
AmbroseByamugisha <ambrosebyamugisha@gmail.com>
2019-12-03 18:20:41 +0300

    add the datastructures

---
+++ onlinevote/src/data.js
@@ -0,0 +1,33 @@
+var students = [
+    {
+        "stud_no": 214002125,
+        "reg_no": "14/U/5974/PS"
+    },
+    {
+        "stud_no": 214002126,
+        "reg_no": "14/U/5975/PS"
+    },
+    {
+        "stud_no": 214002127,
+        "reg_no": "14/U/5976/PS"
+    }
+]
+
+var candidates = [
+    {
+        "cand_name": "Ambrose",
+        "post": "president"
+    },
+    {
+        "cand_name": "Byamu",
+        "post": "president"
+    },
+    {
+        "cand_name": "Inno",
+        "post": "grc"
+    },
+    {
+        "cand_name": "Masiko",
+        "post": "grc"
+    }
+]
